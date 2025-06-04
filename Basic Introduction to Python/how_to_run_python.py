eb7f44d1a8b8fbaa8e97b57b61ab15e6e830fc7d
Prince Patra <princepatra9951@gmail.com>
2025-06-05 01:01:36 +0530

    Create how_to_run_python.py

---
+++ Basic Introduction to Python/how_to_run_python.py
@@ -0,0 +1,5 @@
+# - Through IDE like VS Code
+# - Using terminal: python filename.py
+# - Interactive shell (type `python` in terminal)
+
+print("Run this script using: python 01_running_python.py")
