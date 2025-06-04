3928b28a180aa31948238c8bcba81c6a30cf7e92
Prince Patra <princepatra9951@gmail.com>
2025-06-05 01:02:14 +0530

    Create escape_sequences.py

---
+++ Basic Introduction to Python/escape_sequences.py
@@ -0,0 +1,4 @@
+print("Line1\nLine2")             # New line
+print("Tabbed\tText")             # Tab space
+print("This is a backslash: \\")  # Backslash
+print("He said, \"Python is fun!\"")  # Quotes inside quotes
