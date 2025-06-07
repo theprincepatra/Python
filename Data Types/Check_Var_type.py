216989b206e03b69c99e7f8bdea0a917fae6252e
Prince Patra <princepatra9951@gmail.com>
2025-06-07 16:11:58 +0530

    Create Check_Var_type.py

---
+++ Data Types/Check_Var_type.py
@@ -0,0 +1,15 @@
+a = input("enter the word of a: ")
+b= int(input("enter a number for b: "))
+c= float(input("enter a number for c: "))
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+# output:
+# Enter the word for a: hello
+# Enter a number for b: 10
+# Enter a number for c: 3.14
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
