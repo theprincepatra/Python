85c7ea9ed74b10819e2eafb01c76e77e388c3741
Prince Patra <princepatra9951@gmail.com>
2025-06-07 16:16:13 +0530

    Update Average_of_num.py

--- Data Types/Average_of_num.py
+++ Data Types/Average_of_num.py
@@ -7,3 +7,12 @@ print("the average of these two numbers is",(a+b)/2)
 #output:
 #entered numebr by a = 31
 #entered numebr by b = 49
+
+#find square of a two number
+
+a = int(input("enter a number: "))
+print("square of number is ",a*a)
+
+#output:
+# enter a number: 9
+# square of number is 81
