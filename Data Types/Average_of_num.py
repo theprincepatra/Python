c8a205036e5c429901c55089478f7351e2a8c7b8
Prince Patra <princepatra9951@gmail.com>
2025-06-07 16:14:28 +0530

    Create Average_of_num.py

---
+++ Data Types/Average_of_num.py
@@ -0,0 +1,9 @@
+#find average of two numbers
+
+a = int(input("entered numebr by a = "))
+b = int(input("entered numebr by b = "))
+print("the average of these two numbers is",(a+b)/2)
+
+#output:
+#entered numebr by a = 31
+#entered numebr by b = 49
