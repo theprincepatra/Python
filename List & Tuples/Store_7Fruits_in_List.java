9a557126268509f29b16e670155f01ee7fa4a8f1
Prince Patra <princepatra9951@gmail.com>
2025-06-13 01:32:48 +0530

    Create Store_7Fruits_in_List.java

---
+++ List & Tuples/Store_7Fruits_in_List.java
@@ -0,0 +1,19 @@
+#A program to store seven fruits in a list entered by the user.
+
+fruits = []
+
+f1 = input("enter fruit name")
+fruits.append(f1)
+f2 = input("enter fruit name")
+fruits.append(f2)
+f3 = input("enter fruit name")
+fruits.append(f3)
+f4 = input("enter fruit name")
+fruits.append(f4)
+f5 = input("enter fruit name")
+fruits.append(f5)
+f6 = input("enter fruit name")
+fruits.append(f6)
+f7 = input("enter fruit name")
+fruits.append(f7)
+print(fruits)
