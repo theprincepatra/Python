0bc81c93068b6ed790f2871cd85b8a032e75bb88
Prince Patra <princepatra9951@gmail.com>
2025-06-13 01:43:38 +0530

    Create Input_data_in_dict.py

---
+++ Dictionaries & Sets/Input_data_in_dict.py
@@ -0,0 +1,20 @@
+#Create an empty dictionary.Allow four friends to enter their favourite language as value and use key as their names.Assume that the names are unique.
+
+d = {}
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+print(d)
