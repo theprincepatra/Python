a40f119bc91e5709c7ce3ea307744f5e81cb6869
Prince Patra <princepatra9951@gmail.com>
2025-06-13 01:44:57 +0530

    Create check_unique.py

---
+++ Dictionaries & Sets/check_unique.py
@@ -0,0 +1,21 @@
+#If the names of 2 friends are same; what will happen to the program in the previous question
+
+d = {}
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+name = input("enter the name")
+lang = input("enter the language ")
+d.update({name:lang})
+
+print(d)
+#only one time if names of 2 friends are same and the language spoken by the same name at last will be printed .
