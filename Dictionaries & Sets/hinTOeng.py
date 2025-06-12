42365b8db1994cbfd4ab82355a186f40ea8738d2
Prince Patra <princepatra9951@gmail.com>
2025-06-13 01:39:05 +0530

    Create hinTOeng.py

---
+++ Dictionaries & Sets/hinTOeng.py
@@ -0,0 +1,9 @@
+#A program to create a dictionary of Hindi words with values as their English translation.Provide user with an option to look it up.
+
+words = {
+    "madad":"Help",
+    "kursi":"chair",
+    "pyar" :"Love"
+}
+word = input("enter the word u want meaning of :")
+print(words[word])
