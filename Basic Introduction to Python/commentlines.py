a2c2ea4a1efcc889b5d736d071cab848647c83df
Prince Patra <princepatra9951@gmail.com>
2025-07-03 01:48:22 +0530

    Create commentlines.py

---
+++ Basic Introduction to Python/commentlines.py
@@ -0,0 +1,10 @@
+# This is a single-line comment
+print("Hello, Python!")  # Print statement
+
+"""
+This is a multi-line comment or docstring.
+It can be used to explain code in multiple lines.
+Python ignores this at runtime.
+"""
+
+print("Let's start learning Python step by step!")
