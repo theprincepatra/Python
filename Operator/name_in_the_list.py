1fd30184def1e10ad152a1797c4892c02693b3b2
Prince Patra <princepatra9951@gmail.com>
2025-07-03 02:02:31 +0530

    Create name_in_the_list.py

---
+++ Operator/name_in_the_list.py
@@ -0,0 +1,18 @@
+#program which finds out whether a given name is present in a list or not.
+
+# Ans ---
+# signing in 
+L = ["Spy","EvilKr4tos","Bombastic","Futuror"]
+
+name = input("enter your name:")
+
+if(name in L):
+    print("Your name in the list")
+
+else:
+    print("Get Out")
+print("END OF PROGRAM")
+# Output:
+# enter your name:Bombastic  
+# Your name in the list  
+# END OF PROGRAM
