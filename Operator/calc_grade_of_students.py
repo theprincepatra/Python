fdbc74ea505086f863b0a193d141c13fc6d476c3
Prince Patra <princepatra9951@gmail.com>
2025-07-03 02:04:46 +0530

    Create calc_grade_of_students.py

---
+++ Operator/calc_grade_of_students.py
@@ -0,0 +1,30 @@
+#write a program to calculate the grade of a student from his marks from the following scheme:
+# 90 - 100 => Ex
+# 80 - 90 => A
+# 70 - 80 => B
+# 60 - 70 => C
+# 50 - 60 => D
+#  <50    => F
+
+# Ans --- 
+# Signing in
+marks = int(input("enter ur marks:"))
+
+if(marks<=100 and marks>=90):
+    print("Ex",marks)
+elif(marks<=90 and marks>=80):
+    print("A",marks)
+elif(marks<=80 and marks>=70):
+    print("B",marks)
+elif(marks<=70 and marks>=60):
+    print("C",marks)
+elif(marks<=60 and marks>=50):
+    print("D",marks)
+else:
+    print("F",marks)
+
+print("END OF PROGRAM")
+# OUTPUT:
+# enter ur marks:85
+# A 85
+# END OF PROGRAM
