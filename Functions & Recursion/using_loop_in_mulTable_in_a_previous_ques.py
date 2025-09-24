43e35fc0949f41ce3265947de04d2cf95660ad52
Prince Patra <princepatra9951@gmail.com>
2025-09-24 18:29:56 +0530

    Create using_loop_in_mulTable_in_a_previous_ques.py

---
+++ Functions & Recursion/using_loop_in_mulTable_in_a_previous_ques.py
@@ -0,0 +1,24 @@
+#  Attempt the number 1 question using while loop . 
+
+
+n = int(input("enter a number:"))
+i = 1 
+while(i<11):
+    print(f"{n} x {i} = {n*i}")
+    i = i+1
+
+print("end of program")
+
+# Output:
+# enter a number:5
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# 5 x 4 = 20
+# 5 x 5 = 25
+# 5 x 6 = 30
+# 5 x 7 = 35
+# 5 x 8 = 40
+# 5 x 9 = 45
+# 5 x 10 = 50
+# end of program
