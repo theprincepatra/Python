89643da80f0eb168c1c0e4589c06e9979b7fb507
Prince Patra <princepatra9951@gmail.com>
2025-09-24 18:34:07 +0530

    Create print_star_pattern.py

---
+++ Functions & Recursion/print_star_pattern.py
@@ -0,0 +1,20 @@
+# Q7 --- Write a program to print the following star pattern.
+'''
+  *
+ ***
+***** for n = 3
+'''
+
+# Ans --- 
+# signing in
+n = int(input("enter a number:"))
+for i in range(1, n+1):
+    print(" "* (n-i), end="")
+    print("*"* (2*i-1), end = "")
+    print("")
+
+# Output:
+# enter a number:3
+#   *
+#  ***
+# *****
