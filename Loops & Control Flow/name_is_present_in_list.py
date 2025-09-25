4499a9c77a596651c1be3e2dbb85e30672e62208
Prince Patra <princepatra9951@gmail.com>
2025-09-25 20:27:40 +0530

    Create name_is_present_in_list.py

---
+++ Loops & Control Flow/name_is_present_in_list.py
@@ -0,0 +1,20 @@
+# Write a program which finds out whether a given name is present in a list or not.
+
+# Ans ---
+# signing in 
+L = ["Spy","TPP","Bombastic","King"]
+
+name = input("enter your name:")
+
+if(name in L):
+    print("Your name in the list")
+
+else:
+    print("Get Out")
+print("END OF PROGRAM")
+# signing off     
+
+# Output:
+# enter your name: TPP
+# Your name in the list
+# END OF PROGRAM
