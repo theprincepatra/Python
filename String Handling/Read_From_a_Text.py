48350f1639da5ecbe992c170ead6c8ef94c69768
Prince Patra <princepatra9951@gmail.com>
2025-10-07 21:27:46 +0530

    Create Read_From_a_Text.py

---
+++ String Handling/Read_From_a_Text.py
@@ -0,0 +1,13 @@
+# Write a program to read a text from a given file 'poem.txt'and find out whether it contains the word 'twinkle'.
+
+# Ans ---
+f = open("poem.txt")
+if ("twinkle in content"):
+    print("The word twinkle is present in the content")
+
+else:
+    print("BYE")
+f.close()
+
+# Output:
+# The word twinkle is present in the content
