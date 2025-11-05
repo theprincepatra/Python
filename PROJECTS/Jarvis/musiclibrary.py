f0a3f925cafbcbec2658f12960d82cf190cc7504
Prince Patra <princepatra9951@gmail.com>
2025-11-05 09:15:42 +0530

    Create musiclibrary.py

---
+++ PROJECTS/Jarvis/musiclibrary.py
@@ -0,0 +1,16 @@
+music = {
+    "agar tum sath ho": "https://youtu.be/xRb8hxwN5zc?si=2SDj1xjbx3R551qw",
+    "kal  ho naa ho": "https://youtu.be/g0eO74UmRBs?si=m-v-te2CxWW5C4fH",
+    "dil ibadat": "https://youtu.be/U2QNhsAgIIE?si=3ZHjGb_ZLXo3YnxC",
+    "chiknni chameli": "https://youtu.be/MQM7CNoAsBI?si=EgGI6Ztuc9ICOUfN",
+    "chi chi chi re nani chi": "https://youtu.be/I2bvoO0CoDQ?si=bDe4R3iNR9Vh2piA",
+    "tinku jiya": "https://youtu.be/k0FNdxN0pV8?si=BaWU1iVqULGBI_zu",
+    "kya hua teera vada": "https://youtu.be/Z5D1dhTMclI?si=hqSmwfwKoyHdp-WD",
+    "tere mast mast do nain": "https://youtu.be/oyLVu753XJw?si=OHmkN2m93LQOrEmy",
+    "tum hi ho": "https://youtu.be/Umqb9KENgmk?si=gKQcT49x07wYKl2N",
+    "baarishein": "https://youtu.be/PJWemSzExXs?si=OxOq2VcWW35mICB9",
+    "party all night": "https://youtu.be/Wd7H311MrWA?si=qY7yis5ywI9e5ToM",
+    "one love": "https://youtu.be/0pWsCiBvLOk?si=L8M4yzDMBq2CWHkI",
+    "bulleya": "https://youtu.be/a9Hxkc9YxGE?si=QKndFFj4hFliF-Fp",
+    "sad bulleya": "https://youtu.be/wTgrZE9RWNY?si=g-k3PC7rGaCGwZ3b"
+}
