4f00dd783147880663e99263d1d41ac5462e6fe6
Prince Patra <princepatra9951@gmail.com>
2025-11-05 08:16:20 +0530

    Create 12)str_method.py

---
+++ OOPS/12)str_method.py
@@ -0,0 +1,39 @@
+# Write __str__() method to print the vector as follows: 7i + 8j + 10k
+
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        result = Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        return result
+
+    def __mul__(self, other):
+        result = self.x * other.x + self.y * other.y + self.z * other.z
+        return result
+
+    def __str__(self):
+        return f"{self.x}i + {self.y}j + {self.z}k"
+
+
+# Test the implementation
+v1 = Vector(1, 2, 3)
+v2 = Vector(4, 5, 6)
+v3 = Vector(7, 8, 9)
+
+print(v1 + v2)  # Output: 5i + 7j + 9k
+print(v1 * v2)  # Output: 32
+
+print(v1 + v3)  # Output: 8i + 10j + 12k
+print(v1 * v3)  # Output: 50
+
+print("End of program")
+
+# Output:
+# 5i + 7j + 9k
+# 32
+# 8i + 10j + 12k
+# 50
+# End of program
